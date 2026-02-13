75fdd52803b83fcb8bf566a7aa26e2887184e3d7
Emam saimon <likonsaimon123@gmail.com>
2026-02-13 12:15:50 +0600

    connect to mongo Atlas

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const DB_NAME = "backend01";
+// export const DB_URL = process.env.MONGO_URI;
+// export const PORT = process.env.PORT || 8000;
+
+ 
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env"
+});
+
+ connectDB();
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app = express();
+
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("our application is not able to talk the Database", error);
+      throw error;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();
+*/
