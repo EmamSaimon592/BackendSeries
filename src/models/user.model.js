0baf2425e9f05eeb0cf9022bffade64f41e1c0ea
Emam saimon <likonsaimon123@gmail.com>
2026-02-16 21:58:56 +0600

    accres_token & Refresh_token genetrate

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,9 +2,6 @@ import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-
-
-
 const userSchema = new Schema(
   {
     username: {
@@ -13,58 +10,60 @@ const userSchema = new Schema(
       unique: true,
       lowercase: true,
       trim: true,
-      index: true
+      index: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     fullName: {
       type: String,
       required: true,
       trim: true,
-      index: true
+      index: true,
     },
     avater: {
       type: String, // cloudinary theke image url asbe
-      required: true
+      required: true,
     },
     coverImage: {
       type: String, // cloudinary theke image url asbe
-      
     },
     watchHistory: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Video"
-      }
+        ref: "Video",
+      },
     ],
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: [6, "Password must be at least 6 characters long"]
+      minlength: [6, "Password must be at least 6 characters long"],
     },
     refreshToken: {
-      type: String
-    }
+      type: String,
+    },
   },
   { timestamps: true }
-  
 );
 
+
+
+
 userSchema.pre("save", async function (next) {
-  this.password = bcrypt.hash(this.password, 10);
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
   next();
-})
+});
 
-
-
-
-
-
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 
 
@@ -74,8 +73,7 @@ userSchema.pre("save", async function (next) {
 const User = mongoose.model("User", userSchema);
 export default User;
 
-
 // ei c line er mane holo je amra mongoose theke model create korchi, jekhane "User" holo model er name ebong userSchema holo schema jeta amra age define korechi. Tarpor amra ei model ke export kore dichi jate onno jaygay use kora jay.
 
-// short version: 
+// short version:
 // export const User = mongoose.model("User", userSchema);
