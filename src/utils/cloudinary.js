a797873024bd79762d1168f7810c20a8a95e0514
Emam saimon <likonsaimon123@gmail.com>
2026-02-17 17:46:59 +0600

    defined cloudinary configaration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localfilePath) => {
+   try {
+     if (!localfilePath) return null;
+     // Upload the image to Cloudinary
+     const response =  await cloudinary.uploader.upload(localfilePath, {
+       resource_type: "auto"
+     })
+     // file uploaded successfully
+     console.log("File uploaded successfully on cloudinary", response.url);
+     return response;
+    
+   } catch (error) {
+    fs.unlinkSync(localfilePath); // Delete the local file after upload attempt
+     return null;
+   }
+};
+
+export default uploadOnCloudinary;
+
+
+
+
+/*
+// example of uploading an image from a URL to cloudinary
+cloudinary.v2.uploader.upload("https:upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result, error);
+  }
+);
+*/
